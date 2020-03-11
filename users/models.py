8a1b78228845c92bfd1ab394a78ab9b8aa4fc6da
Bjenk Ellefsen <bjenk.ellefsen@canada.ca>
2020-03-11 19:32:28 -0400

    Changes to landing page, profile card in Central section

--- users/models.py
+++ users/models.py
@@ -94,7 +94,7 @@ class Profile(models.Model):
 
         img = Image.open(self.image.path)
 
-        if img.height > 400 or img.width > 500:
-            output_size = (350, 450)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
